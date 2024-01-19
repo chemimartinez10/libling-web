3a23f9a5e54d880c57cd55fe98eaf9bf3d9101a3
Chemi Martinez <jmmartineztuarez@gmail.com>
2024-01-19 09:58:07 -0400

    feat: :sparkles: align button

--- src/app/components/article.tsx
+++ src/app/components/article.tsx
@@ -58,7 +58,7 @@ const Article: React.FC<IArticle> = ({ title, subtitle, content, list, Icon, but
             }
             {
                 buttonText &&
-                <Link href={buttonAction} style={{alignSelf:'left'}}>
+                <Link href={buttonAction} style={{alignSelf:'center'}}>
                     <Button text={buttonText}/>
                 </Link>
             }
