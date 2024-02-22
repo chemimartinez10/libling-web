ad7700b85d3f0f5a03cebe7bfadd2e99e4f6c908
Chemi Martinez <jmmartineztuarez@gmail.com>
2024-02-22 12:56:53 -0400

    feat: :sparkles: updated seeder

--- prisma/seeder.ts
+++ prisma/seeder.ts
@@ -6,14 +6,34 @@ const prisma = new PrismaClient()
 
 export async function main() {
 	try {
-		const user = await prisma.user.create({
-			data: {
+		const users: {
+			id: number
+			email: string
+			name: string | null
+			password: string
+		}[] = []
+		const data = [
+			{
 				name: "Chemi",
 				email: "chemi@huskydevs.com",
 				password: await bcrypt.hash("chemitofeliz", saltRounds),
 			},
+			{
+				name: "Javier",
+				email: "nunezgomezjavier@gmail.com",
+				password: await bcrypt.hash("password", saltRounds),
+			},
+			{
+				name: "Lidiz",
+				email: "lidizcruz@libling.lu",
+				password: await bcrypt.hash("Libling.123", saltRounds),
+			},
+		]
+		data.forEach(async (el) => {
+			let user = await prisma.user.create({ data: el })
+			users.push(user)
 		})
-		console.log(user)
+		console.log(users)
 		await prisma.$disconnect()
 	} catch (e) {
 		console.error(e)
