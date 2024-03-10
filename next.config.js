2c8dce0f9117656ef198117be3e41ad13a7408b7
Chemi Martinez <jmmartineztuarez@gmail.com>
2024-03-09 23:31:45 -0400

    feat: :sparkles: prod text

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        unoptimized: false
+        unoptimized: false,
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'libling-files.public.blob.vercel-storage.com',
+              port: '',
+            },
+          ],
     }
 }
 
