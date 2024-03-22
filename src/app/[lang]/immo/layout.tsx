5291d1fcfcf51bdd5c83bbfe42d1801799502fcf
Chemi Martinez <jmmartineztuarez@gmail.com>
2024-03-22 12:54:27 -0400

    feat: :sparkles: more fixes

--- src/app/[lang]/immo/layout.tsx
+++ src/app/[lang]/immo/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from 'next'
 import '../../globals.css'
 import { poppinsRegular } from '../../fonts'
-import Footer from '@/app/components/footer'
 import styles from '@/app/[lang]/(landing)/page.module.css'
-import Menu from '@/app/components/menu'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Script from 'next/script'
 import NavbarImmo from '@/app/components/navbarImmo'
 import ContactSection from '@/app/components/contactSection'
 import FooterImmo from '@/app/components/footerImmo'
-import { getCookie } from 'cookies-next'
 import { countryType } from '@/app/interfaces'
 import { cookies } from 'next/headers'
 import ContactForm from '@/app/components/contactForm'
@@ -23,6 +19,15 @@ export const metadata: Metadata = {
   description: 'Get your new property with Libling Immo a Libling solution to find a property, get a new life in luxemburg, and search for properties arround the world',
   robots:'index, follow',
   keywords:'real state, immobilier, rent, sale, houses, libling, live, immo',
+  alternates:
+  {
+    canonical: "https://libling.lu/immo",
+    languages: {
+      en: "https://libling.lu/en/immo",
+      es: "https://libling.lu/es/immo",
+      fr: "https://libling.lu/fr/immo",
+    }
+  }
 }
 
 export default function RootLandingLayout({
