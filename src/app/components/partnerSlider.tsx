4e337812574a6da5594e86b5520bc40a1cf8e46a
Chemi Martinez <jmmartineztuarez@gmail.com>
2024-06-03 14:14:07 -0400

    logos fix

--- src/app/components/partnerSlider.tsx
+++ src/app/components/partnerSlider.tsx
@@ -3,22 +3,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 import styles from './partnerSlider.module.css'
 import Image from 'next/image';
-import LogoApple from '@/app/img/logo_apple.png'
-import LogoBinance from '@/app/img/logo_binance.png'
-import LogoCosmos from '@/app/img/logo_cosmos.png'
-import LogoEtherium from '@/app/img/logo_ethereum_foundation.png'
-import LogoGoogle from '@/app/img/logo_google.png'
-import LogoMeta from '@/app/img/logo_meta.png'
-import LogoMicrosoft from '@/app/img/logo_microsoft.png'
-import Logo1 from '@/app/img/logo_partner_assurin.jpg'
-import Logo2 from '@/app/img/logo_partner_chamber_of_commerce_luxembourg.jpg'
-import Logo3 from '@/app/img/logo_partner_elena_lustina_panescu.jpg'
-import Logo4 from '@/app/img/logo_partner_house_of_training.jpg'
-import Logo5 from '@/app/img/logo_partner_husky_devs@2x.jpg'
-import Logo6 from '@/app/img/logo_partner_lalux_assurances.jpg'
-import Logo7 from '@/app/img/logo_partner_schemberger_photographe.jpg'
-import Logo8 from '@/app/img/logo_partner_translations_panescu.jpg'
-import Logo9 from '@/app/img/logo_partner_zak_architect.jpg'
+import Logo1 from '@/app/img/logo_partner_assurin-100.jpg'
+import Logo2 from '@/app/img/logo_partner_chamber_of_commerce_luxembourg-100.jpg'
+import Logo3 from '@/app/img/logo_partner_elena_lustina_panescu-100.jpg'
+import Logo4 from '@/app/img/logo_partner_house_of_training-100.jpg'
+import Logo5 from '@/app/img/logo_partner_husky-100.jpg'
+import Logo6 from '@/app/img/logo_partner_lalux-100.jpg'
+import Logo7 from '@/app/img/logo_partner_schemeberger_photographe-100.jpg'
+import Logo8 from '@/app/img/logo_partner_translations_panescu-100.jpg'
+import Logo9 from '@/app/img/logo_partner_zak_architect-100.jpg'
 
 const PartnerSlider = () => {
   return (
